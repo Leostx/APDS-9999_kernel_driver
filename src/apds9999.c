0bc25fa136e490217323bcb9bb15484403d5500b
Leostx <leo@stexo.eu>
2026-08-17 21:21:37 +0200

    Fixed read_avail bug

--- src/apds9999.c
+++ src/apds9999.c
@@ -343,6 +343,8 @@
 	.scan_type               = APDS9999_INTENSITY_SCAN_TYPE,		                        \
 	.info_mask_separate      = BIT(IIO_CHAN_INFO_RAW) |	BIT(IIO_CHAN_INFO_PROCESSED),       \
 	.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_HARDWAREGAIN) | BIT(IIO_CHAN_INFO_SCALE), \
+	/* The following is for having the *_available file in the sysfs dir that runs read_avail when read */  \
+	.info_mask_shared_by_type_available = BIT(IIO_CHAN_INFO_HARDWAREGAIN),                  \
 }
 
 /* ------------------- END IIO CHANNEL DEFINES ------------------- */
@@ -617,6 +619,7 @@ static const struct iio_chan_spec apds9999_channels[] = {
 		.scan_type               = APDS9999_INTENSITY_SCAN_TYPE,
 		.info_mask_separate      = BIT(IIO_CHAN_INFO_RAW) | BIT(IIO_CHAN_INFO_PROCESSED),
 		.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_HARDWAREGAIN) | BIT(IIO_CHAN_INFO_SCALE),
+		.info_mask_shared_by_type_available = BIT(IIO_CHAN_INFO_HARDWAREGAIN),
 	},
 
 };
